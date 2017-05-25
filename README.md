0e8c466ee48af773d9708e9f93f9f6d0737505c4
bogdanprofirescu <bogdanprofirescu@yahoo.com>
2017-05-25 20:24:37 +0300

    Updated as per feedback

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-This app reads feeds from a number of sources.
+**This app reads feeds from a number of sources.**
 
-Click the "hamburger" menu on the top left of the page to chose the themes you are interested in: Udacity blog, CSS Tricks, HTML5 or Linear Digressions.
+**How to install the application locally**
+  1.  Download(green button on top-right) the application from    https://github.com/bogdanprofirescu/Feedreader_Jasmine_Tested  
+  and unzip the file on a local folder.
 
-After the selection the page will refresh and render a list of articles within that particular theme.
+  2. Please install the free static webserver Fenix, from http://fenixwebserver.com/.  
+      Click donwnload from the homepage, and install with all default options.
+      Run the Fenix server.
+      From the Web Server Menu, click New Server.
+      In the new window pick a name for the server and indicate the path to
+      the folder where you unzipped the download.
+      Hit create button.
+      The server will be created in the Fenix window.
+      Click the new server name and the default browser will open the app.
 
-Simply click on any of the links on the page to read the full articles on their original pages.
+**How to use the application**
+1. Click the "hamburger" menu on the top left of the page to chose the themes you are interested in: Udacity blog, CSS Tricks, HTML5 or Linear Digressions.
+
+2. After the selection the page will refresh and render a list of articles within that particular theme.
+
+3. Simply click on any of the links on the page to read the full articles on their original pages.
+
+**How to see the Jasmine tests results**
+
+1. Check the text at the bottom of the page after loading.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             }
         });
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -51,10 +51,10 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    /*  Write a new test suite named "The menu" */
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /*  Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -65,7 +65,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that ensures the menu changes
+        /*  Write a test that ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -81,27 +81,27 @@ $(function() {
         });
 
 
-        /* TODO: Write a new test suite named "Initial Entries" */
+        /*  Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /*  Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-        /* TODO: Write a new test suite named "New Feed Selection" */
+        /*  Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /*  Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /*  Write a new test suite named "Initial Entries" */
 
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /*  Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -116,28 +116,30 @@ $(function() {
         });
 
 
-        /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /*  Write a new test suite named "New Feed Selection" */
 
-    describe('New Feed Selection', function() {
+    describe('New Feed Selection', function(done) {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+      /*  Write a test that ensures when a new feed is loaded
+      * by the loadFeed function that the content actually changes.
+      * Remember, loadFeed() is asynchronous.
+      */
+        var initialFeed;
         beforeEach(function(done) {
-            loadFeed(0, done);
+            loadFeed(1,function(){
+                initialFeed = $('.feed').html();
+                done();
+            });
         });
-        //check if the h2 size indicates that there is content/content different than ''
-        it('content changed on feed load', function() {
-            expect($('div.feed .entry h2').text).not.toBe('');
+
+        it('content changed on feed load', function (done) {
+            loadFeed(2,function(){
+                expect($('.feed').html()).not.toEqual(initialFeed);
+                done();
+            })
         });
 
     });
